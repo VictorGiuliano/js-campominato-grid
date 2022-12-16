58273bba13b855af8ec91dc045a0c31a4ab94f5e
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-16 18:03:19 +0100

    Create cell

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console
  con il numero della cella cliccata.*/
+
+ const zone = document.getElementById('grid');
+
+ const col = 10;
+ const rows = 10;
+ const total = col * rows;
+
+ //Creazione delle celle
+ let cell = ` `;
+ for(let i=0;i<total;i++){
+    cell = document.createElement("div");
+    cell.classList.add('cell');
+    zone.appendChild(cell);
+ }
+ cell.innerHtml = "div";
+ 
